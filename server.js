92e1b91ff5fae7993b9f7dd7c4b9841d0d3a163f
William Rigaut <rigautwilliam1@gmail.com>
2025-04-22 16:38:13 +0200

    Revert test options MongoClients & MAJ Package mongodb@latest

--- server.js
+++ server.js
@@ -39,7 +39,13 @@ console.log('--- NOUVEAU DEBUG --- Valeur BRUTE de process.env.MONGO_URI :', raw
 const mongouri = String(rawMongoUri || '').trim(); 
 
 console.log('--- NOUVEAU DEBUG --- Valeur de mongouri APRES nettoyage (trim) :', mongouri); // Log après nettoyage
-const mongoClient = new MongoClient(mongouri); // Retire complètement le deuxième argument des options
+const mongoClient = new MongoClient(mongouri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
 let dbMongo;
 
